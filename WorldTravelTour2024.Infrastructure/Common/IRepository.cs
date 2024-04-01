ea9ab216b6bc1f5e1802ae4e2f963e1fe3ca5df7
Станимир Стойчев Стоев <stanstz@abv.bg>
2024-04-01 15:16:47 +0300

    Creating Repository, so to be possible t owork with the data from the database

---
+++ WorldTravelTour2024.Infrastructure/Common/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace WorldTravelTour2024.Infrastructure.Common
+{
+    public interface IRepository
+    {
+        IQueryable<T> All<T>() where T : class;
+
+        IQueryable<T> AllReadOnly<T>() where T : class;
+    }
+}
