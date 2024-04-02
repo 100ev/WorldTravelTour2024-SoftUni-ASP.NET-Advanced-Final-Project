9a82221361aee33274bbf1ea6caad87ed393283d
Станимир Стойчев Стоев <stanstz@abv.bg>
2024-04-02 21:21:59 +0300

    Implementing the methods in TransportationProviderService

--- WorldTravelTour2024.Core/Contracts/TransportationProvider/ITransportationProviderService.cs
+++ WorldTravelTour2024.Core/Contracts/TransportationProvider/ITransportationProviderService.cs
@@ -8,5 +8,12 @@ namespace WorldTravelTour2024.Core.Contracts.TransportationProvider
 {
     public interface ITransportationProviderService
     {
+        public Task<bool> ExistAsync(string userId);
+        public Task UpdatePersonalInformationAsync(string userId,string phoneNumber);
+        public Task<bool> CanTransportTravellerToCountryAsync(string country);
+        public Task NumberOfTravellersAllowedToTransportAsync(int numTravellers);
+        public Task RegisterTransportationProviderAsync(string userId, string firstName, string lastName, string phoneNumber);
+        public Task RemoveAsync(string id);
+        public Task<decimal> ReceiveProfitAsync();
     }
 }
