42ba9b41c2869afa72eebaf4e9f7c10de6de9d2b
Станимир Стойчев Стоев <stanstz@abv.bg>
2024-04-23 16:23:37 +0300

    Adding configuration for CountryInformationAdvisor

---
+++ WorldTravelTour2024.Infrastructure/Data/SeedDb/CountryInformationAdvisorConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WorldTravelTour2024.Infrastructure.Data.Models;
+
+namespace WorldTravelTour2024.Infrastructure.Data.SeedDb
+{
+    public class CountryInformationAdvisorConfiguration : IEntityTypeConfiguration<CountryInformationAdvisor>
+    {
+        public void Configure(EntityTypeBuilder<CountryInformationAdvisor> builder)
+        {
+            var data = new SeedDatabase();
+            builder.HasData(new CountryInformationAdvisor[] { data.CountryInformationAdvisor });
+        }
+    }
+}
