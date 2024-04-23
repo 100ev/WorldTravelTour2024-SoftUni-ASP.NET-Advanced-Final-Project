915e8ecb64ad0ff41774cd7081c1634361be6312
Станимир Стойчев Стоев <stanstz@abv.bg>
2024-04-23 19:36:14 +0300

    Creating sevice model for CountryInformationAdvisor and implementing the methods

---
+++ WorldTravelTour2024.Core/Contracts/CountryInformationAdvisor/ICountryInformationAdvisorService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorldTravelTour2024.Core.Models.CountryInformationAdvisor;
+
+namespace WorldTravelTour2024.Core.Contracts.CountryInformationAdvisor
+{
+    public interface ICountryInformationAdvisorService
+    {
+        public Task<bool> ExistAsync(int id);
+        public Task<CountryInformationFormModel> AssistTravellerAsync(int travellerID, int continentId, string country);
+        public Task<CountryInformationFormModel> AssistTransportationProviderAsync(int transportationProviderId,int continentId, string country);
+
+    }
+}
---
+++ WorldTravelTour2024.Core/Services/CountryInformationAdvisor/CountryInformationAdvisorService.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorldTravelTour2024.Core.Contracts.CountryInformationAdvisor;
+using WorldTravelTour2024.Core.Models.CountryInformationAdvisor;
+using WorldTravelTour2024.Infrastructure.Common;
+
+namespace WorldTravelTour2024.Core.Services.CountryInformationAdvisor
+{
+    public class CountryInformationAdvisorService : ICountryInformationAdvisorService
+    {
+        private readonly IUniversalRepository _repository;
+
+        public CountryInformationAdvisorService(IUniversalRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<CountryInformationFormModel> AssistTransportationProviderAsync(int transportationProviderId,int continentId, string country)
+        {
+            var transportationProvider = await _repository.AllReadOnly<WorldTravelTour2024.Infrastructure.Data.Models.TransportationProvider>()
+                .FirstOrDefaultAsync(tp => tp.Id == transportationProviderId);
+
+            var continent = await _repository.AllReadOnly<WorldTravelTour2024.Infrastructure.Data.Models.Continent>()
+                .FirstOrDefaultAsync(c => c.Id == continentId);
+
+            if (transportationProvider != null && continent != null)
+            {
+                if (continent.Countries.FirstOrDefault(x => x.Name == country) != null)
+                {
+                    foreach (var item in continent.Countries)
+                    {
+                        if (item.Name == country)
+                        {
+                            var model = new CountryInformationFormModel()
+                            {
+                                Id = 1,
+                                UserToAssist = $"{transportationProvider.FirstName} {transportationProvider.LastName}",
+                                Country = item.Name,
+                                Description = item.Description
+                            };
+
+                            return model;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public async Task<CountryInformationFormModel> AssistTravellerAsync(int travellerID,int continentId, string country)
+        {
+            var traveller = await _repository.AllReadOnly<WorldTravelTour2024.Infrastructure.Data.Models.Traveller>()
+                .FirstOrDefaultAsync(t => t.Id == travellerID);
+
+            var continent = await _repository.AllReadOnly<WorldTravelTour2024.Infrastructure.Data.Models.Continent>()
+                .FirstOrDefaultAsync(c => c.Id == continentId);
+
+            if (traveller != null && continent != null)
+            {
+                if (continent.Countries.FirstOrDefault(x => x.Name == country) != null)
+                {
+                    foreach (var item in continent.Countries)
+                    {
+                        if (item.Name == country)
+                        {
+                            var model = new CountryInformationFormModel()
+                            {
+                                Id = 1,
+                                UserToAssist = $"{traveller.FirstName} {traveller.LastName}",
+                                Country = item.Name,
+                                Description = item.Description
+                            };
+
+                            return model;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public async Task<bool> ExistAsync(int id)
+        {
+            return id == 1;
+        }
+    }
+}
